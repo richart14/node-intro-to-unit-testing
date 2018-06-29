9aea43d4755f9e40f0fa6d470fa1768c301aa9e8
Richard Tom <tomrichard14@gmail.com>
2018-06-28 23:01:04 -0700

    finished node-intro-to-unit-testing

--- test/test-adder.js
+++ test/test-adder.js
@@ -1,8 +1,42 @@
+'use strict';
+
 // import chai, declare expect variable
 const expect = require('chai').expect;
 
 // import adder
 const adder = require('../adder');
+const fizzBuzzer = require('../fizzBuzzer');
+
+describe('fizzBuzzer', function() {
+  it('should display fizz, buzz, or fizz-buzz or number', function() {
+    const normalCases = [
+      {num: 5, expected: 'buzz'},
+      {num: 22, expected: 22},
+      {num: 30, expected: 'fizz-buzz'},
+      {num: 6, expected: 'fizz'},
+    ];
+
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.num);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should display fizz, buzz, or fizz-buzz or number', function() {
+    const badInputs = [
+      {num: 'a', expected: 5},
+      {num: true, expected: 'fizz-buzz'},
+      {num: false, expected: 'fizz-buzz'},
+      {num: 'bea', expected: 'fizz'},
+    ];
+
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizzBuzzer(input.num);
+      }).to.throw(Error);
+    });
+  });
+});
 
 // unit tests for our `adder` function
 describe('adder', function() {
